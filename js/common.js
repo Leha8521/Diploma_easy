c38c36acfad48d1c54046c046af8b7404beff864
Alexey <leha8521@mail.ru>
2018-04-20 00:26:28 +0500

    Второй день работы над проектом

--- js/common.js
+++ js/common.js
@@ -21,8 +21,64 @@ let overlay = document.getElementsByClassName('overlay')[0],
 
 
 
+        let name = document.getElementById('name'),
+            age = document.getElementById('age'),
+            bio = document.getElementById('bio'),
+            female = document.getElementById('female'),
+            textBio = document.getElementById('bio');
+                  if (window.performance) {
+                   name.value = '';
+                   age.value = '';
+                   bio.value = '';
+                   female.checked = true;
+                   textBio = '';
+
+                    }
+
+
     });
 
+  
+// Customization
 
+let ready = document.getElementById('ready'),
+
+    name = document.getElementById('name'),
+    age = document.getElementById('age'),
+    //sexRadio = document.getElementsByClassName('radio')[0],
+    male = document.getElementById('male'),
+    female = document.getElementById('female'),
+    select = document.getElementById('select'),
+    textBio = document.getElementById('bio'),
+
+    main_cards = document.getElementsByClassName('main-cards-item-active')[0],
+    nameCards = document.getElementsByClassName('name')[0],
+    ageCards = document.getElementsByClassName('age')[0],
+    sex = document.getElementsByClassName('sex')[0],
+    views = document.getElementsByClassName('views')[0],
+    bio = document.getElementsByClassName('bio')[0];
+        
+        ready.addEventListener('click', function() {
+            custom_overlay.style.display = 'none';
+            custom_info.style.display = 'none';
+            custom_char.style.display = 'none';
+            custom_style.style.display = 'none';
+            main.style.display = 'block';
+
+
+            nameCards.textContent = name.value;
+            ageCards.textContent = age.value;
+            
+                if (male.checked) {
+                    sex.textContent = male.value;
+                } else {
+                    sex.textContent = female.value;
+                }
+
+            views.textContent = select.value;
+            bio.textContent = textBio.value;
+
+
+        });
 
 });
